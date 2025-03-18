c8fc73d6b3ab33a07963a60a709682f9f87027fa
Derek Chen <derex1987@gmail.com>
2025-03-17 20:27:13 -0500

    feat: leaflet bounds

--- docs/minimap.js
+++ docs/minimap.js
@@ -1,6 +1,8 @@
 var map = L.map('map', {
     crs: L.CRS.Simple,
-    minZoom: 0
+    minZoom: 0,
+    maxBounds: [[0, 0], [886, 1773]],
+    maxBoundsViscosity: 1.0
   });
   
   var imageUrl = '/docs/pl3xmap.png'; 
