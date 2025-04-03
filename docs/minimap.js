4e7b71eb758b2a7e0d862447ca79197759624450
Derek Chen <derex1987@gmail.com>
2025-04-03 15:16:26 -0500

    feat: bigger preview map hack

--- docs/minimap.js
+++ docs/minimap.js
@@ -5,7 +5,7 @@ var map = L.map('map', {
     maxBoundsViscosity: 1.0
   });
   
-  var imageUrl = 'https://raw.githubusercontent.com/DereC4/derexsmp/refs/heads/main/docs/pl3xmap.png'; 
+  var imageUrl = 'https://raw.githubusercontent.com/DereC4/derexsmp/refs/heads/main/docs/pano4_2025.png'; 
   var imageBounds = [[0, 0], [886, 1773]]; 
   
   L.imageOverlay(imageUrl, imageBounds).addTo(map);
