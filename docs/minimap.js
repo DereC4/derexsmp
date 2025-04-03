cf8d59503750935d84a78b0cceca8b5462096428
Derek Chen <derex1987@gmail.com>
2025-04-03 15:34:38 -0500

    feat: mr beast is on speedway lol

--- docs/minimap.js
+++ docs/minimap.js
@@ -1,13 +1,20 @@
-var map = L.map('map', {
-    crs: L.CRS.Simple,
-    minZoom: 0,
-    maxBounds: [[0, 0], [1939, 3367]],
-    maxBoundsViscosity: 1.0
-  });
-  
-  var imageUrl = 'https://raw.githubusercontent.com/DereC4/derexsmp/refs/heads/main/docs/pano4_2025.jpg'; 
-  var imageBounds = [[0, 0], [1939, 3367]]; 
-  
-  L.imageOverlay(imageUrl, imageBounds).addTo(map);
-  
-  map.fitBounds(imageBounds);
+var map = L.map("map", {
+  crs: L.CRS.Simple,
+  minZoom: 0,
+  maxBounds: [
+    [0, 0],
+    [1939, 3367],
+  ],
+  maxBoundsViscosity: 1.0,
+});
+
+var imageUrl =
+  "https://raw.githubusercontent.com/DereC4/derexsmp/refs/heads/main/docs/pano4_2025.jpg";
+var imageBounds = [
+  [0, 0],
+  [1939, 3367],
+];
+
+L.imageOverlay(imageUrl, imageBounds).addTo(map);
+
+map.setView([1350, 1600], 0);
