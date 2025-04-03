977ef6e04715ddb37be6b76331c08c44a18fd6f4
Derek Chen <derex1987@gmail.com>
2025-04-03 15:20:44 -0500

    feat: whoops it is a jpeg

--- docs/minimap.js
+++ docs/minimap.js
@@ -5,7 +5,7 @@ var map = L.map('map', {
     maxBoundsViscosity: 1.0
   });
   
-  var imageUrl = 'https://raw.githubusercontent.com/DereC4/derexsmp/refs/heads/main/docs/pano4_2025.png'; 
+  var imageUrl = 'https://raw.githubusercontent.com/DereC4/derexsmp/refs/heads/main/docs/pano4_2025.jpg'; 
   var imageBounds = [[0, 0], [3367, 1939]]; 
   
   L.imageOverlay(imageUrl, imageBounds).addTo(map);
